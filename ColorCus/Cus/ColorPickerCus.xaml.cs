1218c0a05ff2f21f53c5faa85860ea09f00b511f
VayneMai <manmuoi021204@gmai.com>
2021-02-27 21:33:11 +0700

    - Update Content

---
+++ ColorCus/Cus/ColorPickerCus.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ColorCus.Cus;
+
+namespace ColorCus.Cus
+{
+	/// <summary>
+	/// Interaction logic for ColorPickerCus.xaml
+	/// </summary>
+	public partial class ColorPickerCus : Window
+	{
+		string[] nameColor = new string[] { "Red", "Yellow", "Maroon", "OrangeRed", "DarkCyan", "DarkOrange", "Magenta", "DeepPink", "RoyalBlue" };
+		List<PropertyInfo> inforProperties = new List<PropertyInfo>();
+		public ColorPickerCus()
+		{
+			InitializeComponent();
+			this.DataContext = this;
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			foreach (var item in typeof(Brushes).GetProperties())
+			{
+				if (nameColor.Contains(item.Name)) { inforProperties.Add(item); }
+			}
+			this.colorList.ItemsSource = null;
+			this.colorList.ItemsSource = inforProperties;
+		}
+		private void colorList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			Brush selectedColor = (Brush)(e.AddedItems[0] as PropertyInfo).GetValue(null, null);
+			MainWindow.instance.btnOpen.Background = selectedColor;
+
+
+			this.Close();
+		}
+		//ColorSelectionCus colorSelectionCus;
+		private void btnColorSelection_Click(object sender, RoutedEventArgs e)
+		{
+			if (MainWindow.instance.ColorPickerCus != null)
+			{
+				MainWindow.instance.ColorPickerCus.Close();
+			}
+			MainWindow.instance.ColorSelectionCus = new ColorSelectionCus();
+			if (MainWindow.instance.ColorSelectionCus.ShowDialog() == true)
+			{
+
+			}
+		}
+	}
+}
---
+++ ColorCus/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ColorCus.Cus;
+
+namespace ColorCus
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public static MainWindow instance;
+		public ColorPickerCus ColorPickerCus;
+		public ColorSelectionCus ColorSelectionCus;
+		public MainWindow()
+		{
+			this.DataContext = this;
+			instance = this;
+			InitializeComponent();
+		}
+
+		private void btnOpen_Click(object sender, RoutedEventArgs e)
+		{
+			ColorPickerCus = new ColorPickerCus();
+			if (ColorPickerCus.ShowDialog() == true)
+			{
+
+			}
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			if (ColorPickerCus!=null)
+			{
+				ColorPickerCus.Close();
+			}
+			if (ColorSelectionCus!=null)
+			{
+				ColorSelectionCus.Close();
+			}
+		}
+	}
+}
